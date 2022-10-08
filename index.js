daf869d61a78d85af2a2019b2ab7db2ada1fa822
ally.c <77428502+RenaudAlly@users.noreply.github.com>
2022-10-07 17:12:16 -0700

    Website design v1 complete. Left to add back-end logic

---
+++ index.js
@@ -0,0 +1,11 @@
+function addOne() {
+    console.log("Button clicked")
+}
+
+function addTwo() {
+    console.log("Button clicked")
+}
+
+function addThree() {
+    console.log("Button clicked")
+}
