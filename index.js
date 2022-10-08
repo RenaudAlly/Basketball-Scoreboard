2accbfdf3470f345fe08ccb174c75ab26d30960c
ally.c <77428502+RenaudAlly@users.noreply.github.com>
2022-10-07 18:34:43 -0700

    Complete first version

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function addOne() {
-    console.log("Button clicked")
+let homeScore = 0
+let guestScore = 0
+
+let homeScoreEl = document.getElementById("homeScore-el")
+let guestScoreEl = document.getElementById("guestScore-el") 
+
+function addOneHome() {
+    homeScore += 1;
+    homeScoreEl.textContent = homeScore;
 }
 
-function addTwo() {
-    console.log("Button clicked")
+function addTwoHome() {
+    homeScore += 2;
+    homeScoreEl.textContent = homeScore;
 }
 
-function addThree() {
-    console.log("Button clicked")
+function addThreeHome() {
+    homeScore += 3;
+    homeScoreEl.textContent = homeScore;
+}
+
+function addOneGuest() {
+    guestScore += 1;
+    guestScoreEl.textContent = guestScore;
+}
+
+function addTwoGuest() {
+    guestScore += 2;
+    guestScoreEl.textContent = guestScore;
+}
+
+function addThreeGuest() {
+    guestScore += 3;
+    guestScoreEl.textContent = guestScore;
 }
