a86177aa70c3a16cbd3efe9af26e81015350d1ab
ally.c <77428502+RenaudAlly@users.noreply.github.com>
2022-10-08 11:50:47 -0700

    Finished adding fouls and New Game button with designing

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let homeScore = 0;
 let guestScore = 0;
+let periodScore = 0;
+let numHomeFouls = 0;
+let numGuestFouls = 0;
 
 let homeScoreEl = document.getElementById("homeScore-el");
 let guestScoreEl = document.getElementById("guestScore-el");
+let periodScoreEl = document.getElementById("periodScore-el");
+let homeFoulsEl = document.getElementById("homeFouls-el");
+let guestFoulsEl = document.getElementById("guestFouls-el");
 
 function addOneHome() {
     homeScore += 1;
@@ -34,11 +40,33 @@ function addThreeGuest() {
     guestScoreEl.textContent = guestScore;
 }
 
-let periodScore = 0;
-let periodScoreEl = document.getElementById("periodScore-el");
-
 function increasePeriod() {
     periodScore += 1;
     periodScore = periodScore % 5; // Wraps around to 0 after hitting 4
     periodScoreEl.textContent = periodScore;
+}
+
+function incrementHomeFoul() {
+    numHomeFouls += 1;
+    homeFoulsEl.textContent = numHomeFouls;
+}
+
+function incrementGuestFoul() {
+    numGuestFouls += 1;
+    guestFoulsEl.textContent = numGuestFouls;
+}
+
+function newGame() {
+    // TODO: Reset the clock timer
+    homeScore = 0;
+    guestScore = 0;
+    periodScore = 0;
+    numHomeFouls = 0;
+    numGuestFouls = 0;
+
+    homeScoreEl.textContent = homeScore;
+    guestScoreEl.textContent = guestScore;
+    periodScoreEl.textContent = periodScore;
+    homeFoulsEl.textContent = numHomeFouls;
+    guestFoulsEl.textContent = numGuestFouls;
 }
